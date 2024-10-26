8a27e3979397cf248642d5dccf411bde94f64713
Thirumalai-Tech-Developer <thirutechdeveloper@gmail.com>
2024-10-26 11:49:27 +0530

    implement Load_Data (Single or Multi) and Function

---
+++ model/PreProcess/Load_Data.py
@@ -0,0 +1,14 @@
+import glob
+from Change_RGB import CandlestickDataset , processor
+from torch.utils.data import Dataset, DataLoader
+# Load Data
+
+csv_files = glob.glob("define_your_data_path")
+dataset = CandlestickDataset(csv_files, processor)
+data_loader = DataLoader(dataset, batch_size=4, shuffle=True)
+
+# for single file (optional)
+
+#csv_files = "define_your_data_path"
+#dataset = CandlestickDataset(csv_files, processor)
+#data_loader = DataLoader(dataset, batch_size=4, shuffle=True)
