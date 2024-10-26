b7f38ca27eee8f9e38043c0eaf1fa81154265479
Thirumalai-Tech-Developer <thirutechdeveloper@gmail.com>
2024-10-26 12:11:32 +0530

    convert into a package

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from model.LoadModel import Load_Model
-from model.
+from model.PreProcess import Change_RGB
+from model.PreProcess import Load_Data
+from model.Train import Train
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
--- model/LoadModel/Load_Model.py
+++ model/LoadModel/Load_Model.py
@@ -1,8 +1,10 @@
 from transformers import BlipProcessor, BlipForConditionalGeneration
 import torch
 
-# Load Your Model
-model = BlipForConditionalGeneration.from_pretrained("Load_your_HuggingFace_Model")
-processor = BlipProcessor.from_pretrained("Load_your_HuggingFace_Model")
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model.to(device)
+class Load_Model:
+    def __init__(self, model_name="your_HuggingFace_Model"):
+        # Load the model and processor from Hugging Face
+        self.model = BlipForConditionalGeneration.from_pretrained(model_name)
+        self.processor = BlipProcessor.from_pretrained(model_name)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
